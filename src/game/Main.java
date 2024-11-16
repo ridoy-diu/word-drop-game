d1643baaadf201ed1d230440ea2f3b3e824fc477
ridoy-diu <ridoy2305101148@diu.edu.bd>
2024-11-16 19:01:24 +0600

    basic GUI formed

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,15 @@
 package game;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JFrame f = new JFrame("Word Drop Game");
+        WordDropGame game = new WordDropGame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(game);
+        f.pack();
+
+        f.setVisible(true);
     }
 }
--- src/game/WordDropGame.java
+++ src/game/WordDropGame.java
@@ -1,7 +1,28 @@
 package game;
 
-public class WordDropGame {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class WordDropGame extends JPanel {
+
+    JButton playButton, pauseButton, playAgainButton;
+    
     WordDropGame() {
-        
+
+        setBackground(Color.DARK_GRAY);
+        setPreferredSize(new Dimension(800, 600));
+
+        playButton = new JButton("Play");
+        pauseButton = new JButton("Pause");
+        playAgainButton = new JButton("Play Again");
+
+        add(playButton);
+        add(pauseButton);
+        add(playAgainButton);
+
+        playButton.setFocusable(false);
+        pauseButton.setFocusable(false);
+        playAgainButton.setFocusable(false);
     }
 }
