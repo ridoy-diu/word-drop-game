1750a981bd4a5c55e0d5397e14b6f15b20a433d9
ri-anik <anik2305101818@diu.edu.bd>
2024-11-18 22:24:46 +0600

    necessary attributes added

--- src/game/WordDropGame.java
+++ src/game/WordDropGame.java
@@ -2,11 +2,25 @@ package game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.random;
 
 
 public class WordDropGame extends JPanel {
 
     JButton playButton, pauseButton, playAgainButton;
+    Timer timer;
+    ArrayList<String> words;
+    ArrayList<Integer> wordX, wordY;
+    Random random;
+    String currentWord = "";
+    int score = 0;
+    boolean gameOver = false;
+
+    String[] wordBank = {"java", "loop", "class", "method", "object", "paint"};
+    int timerDelay = 100;
+
+
     
     WordDropGame() {
 
