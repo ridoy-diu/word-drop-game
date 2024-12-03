f97bd8f18e4aac078adfc128267ea658699f4935
ridoy-diu <ridoy2305101148@diu.edu.bd>
2024-12-03 19:46:00 +0600

    format

--- src/game/WordDropGame.java
+++ src/game/WordDropGame.java
@@ -19,19 +19,29 @@ public class WordDropGame extends JPanel implements ActionListener, KeyListener 
     int lives = 3;
     int timerDelay = 50;
     int scoreThreshold = 100;
-    boolean gameOver = true;
+    boolean gameOver = false;
     String[] wordBank = { "java", "loop", "class", "method", "object", "paint" };
     int wordAddInterval = 1500;
 
     @SuppressWarnings("unused")
     WordDropGame() {
+        setFocusable(true);
+        addKeyListener(this);
+        setBackground(Color.BLACK);
 
-        setBackground(Color.DARK_GRAY);
-        setPreferredSize(new Dimension(800, 600));
+        words = new ArrayList<>();
+        wordX = new ArrayList<>();
+        wordY = new ArrayList<>();
+        random = new Random();
+        timer = new Timer(timerDelay, this);
 
         playButton = new JButton("Play");
         pauseButton = new JButton("Pause");
         playAgainButton = new JButton("Play Again");
+
+        playButton.addActionListener(e -> startGame());
+        pauseButton.addActionListener(e -> pauseGame());
+        playAgainButton.addActionListener(e -> resetGame());
 
         add(playButton);
         add(pauseButton);
@@ -41,40 +51,14 @@ public class WordDropGame extends JPanel implements ActionListener, KeyListener 
         pauseButton.setFocusable(false);
         playAgainButton.setFocusable(false);
 
-        words = new ArrayList<>();
-        wordX = new ArrayList<>();
-        wordY = new ArrayList<>();
-        random = new Random();
-        timer = new Timer(timerDelay, this);
-        playAgainButton.setVisible(true);
+        
+        playAgainButton.setVisible(false);
         pauseButton.setEnabled(false);
 
-        addKeyListener(this);
-        playButton.addActionListener(e -> startGame());
-        pauseButton.addActionListener(e -> pauseGame());
-        playAgainButton.addActionListener(e -> resetGame());
-
-        wordTimer = new Timer(wordAddInterval, e -> addNewWord());
-
-    }
-
-    void addNewWord() {
-        String newWord = wordBank[random.nextInt(wordBank.length)];
-        words.add(newWord);
-        wordX.add(random.nextInt(getWidth() - 100));
-        wordY.add(0);
-    }
-
-    void checkLevelUp() {
-        if (score >= level * scoreThreshold) { 
-            level++; 
-            timerDelay = Math.max(30, timerDelay - 10); 
-            timer.setDelay(timerDelay);  
-            System.out.println("Level Up! Current Level: " + level + " | New Timer Delay: " + timerDelay); 
-        }
+        setPreferredSize(new Dimension(800, 600));
         
+        wordTimer = new Timer(wordAddInterval, e -> addNewWord());
     }
-    
 
     void startGame() {
         timer.start();
@@ -101,11 +85,14 @@ public class WordDropGame extends JPanel implements ActionListener, KeyListener 
         wordX.clear();
         wordY.clear();
         currentWord = "";
+
         timerDelay = 50;
         timer.setDelay(timerDelay);
+
         playButton.setEnabled(true);
         playAgainButton.setVisible(false);
         pauseButton.setEnabled(false);
+
         repaint();
         timer.start();
         wordTimer.start();
@@ -114,6 +101,7 @@ public class WordDropGame extends JPanel implements ActionListener, KeyListener 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
 
@@ -172,69 +160,83 @@ public class WordDropGame extends JPanel implements ActionListener, KeyListener 
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void actionPerformed(ActionEvent e) {
+        if (gameOver)
+            return;
 
+        for (int i = 0; i < wordY.size(); i++) {
+            int fallSpeed = 7;
+            wordY.set(i, wordY.get(i) + fallSpeed);
+
+            if (wordY.get(i) > getHeight()) {
+                words.remove(i);
+                wordX.remove(i);
+                wordY.remove(i);
+                lives--;
+
+                if (lives <= 0) {
+                    gameOver = true;
+                    timer.stop();
+                    wordTimer.stop();
+                }
+            }
+        }
+
+        checkLevelUp();
+        repaint();
+    }
+
+    void checkLevelUp() {
+        if (score >= level * scoreThreshold) {
+            level++;
+            timerDelay = Math.max(30, timerDelay - 10);
+            timer.setDelay(timerDelay);
+            System.out.println("Level Up! Current Level: " + level + " | New Timer Delay: " + timerDelay);
+        }
+    }
+
+    void addNewWord() {
+        String newWord = wordBank[random.nextInt(wordBank.length)];
+        words.add(newWord);
+        wordX.add(random.nextInt(getWidth() - 100));
+        wordY.add(0);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (gameOver) return; 
+        if (gameOver)
+            return;
 
-       char keyChar = e.getKeyChar();
+        char keyChar = e.getKeyChar();
 
-       if (Character.isLetter(keyChar)) { 
-        currentWord += keyChar; }
-
-        for (int i = 0; i < words.size(); i++) { 
-            if (currentWord.equals(words.get(i))) { 
-                words.remove(i); 
-                wordX.remove(i); 
-                wordY.remove(i); 
-                score += 10; 
-                currentWord = ""; 
-                break; 
-            } 
+        if (Character.isLetter(keyChar)) {
+            currentWord += keyChar;
         }
 
-            if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE && currentWord.length() > 0) { 
-                currentWord = currentWord.substring(0, currentWord.length() - 1); 
-            } 
-     
-            repaint();
-        } 
-    
-    
+        for (int i = 0; i < words.size(); i++) {
+            if (currentWord.equals(words.get(i))) {
+                words.remove(i);
+                wordX.remove(i);
+                wordY.remove(i);
+                score += 10;
+                currentWord = "";
+                break;
+            }
+        }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE && currentWord.length() > 0) {
+            currentWord = currentWord.substring(0, currentWord.length() - 1);
+        }
 
+        repaint();
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        if (gameOver) return; 
-        for (int i = 0; i < wordY.size(); i++) { 
-            int fallSpeed = 7; 
-            wordY.set(i, wordY.get(i) + fallSpeed); 
-            if (wordY.get(i) > getHeight()) { 
-                words.remove(i); 
-                wordX.remove(i); 
-                wordY.remove(i); 
-                lives--; 
-                if (lives <= 0) { 
-                    gameOver = true;
-                    timer.stop(); 
-                    wordTimer.stop(); 
-                }
- 
-              
-            }
- 
-            
-        }
-         checkLevelUp(); 
-         repaint();
+    public void keyReleased(KeyEvent e) {
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
 
     public static void main(String[] args) {
