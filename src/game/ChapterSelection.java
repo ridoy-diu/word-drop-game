00dd15c02eb7fcceb660edad78ccdb9f518ce467
ri-anik <anik2305101818@diu.edu.bd>
2024-12-09 23:26:49 +0600

    ChapterSelection Constructor

--- src/game/ChapterSelection.java
+++ src/game/ChapterSelection.java
@@ -4,7 +4,15 @@ import java.awt.*;
 
 public class ChapterSelection extends JPanel {
     private final JFrame frame;
-    private final WordDropGame gamePanel;
+    private final WordDropGame gamePanel
+    ;
+    public ChapterSelection(JFrame frame, WordDropGame gamePanel) {
+        if (frame == null || gamePanel == null) {
+            throw new IllegalArgumentException("Frame and gamePanel cannot be null.");
+        }
+
+        this.frame = frame;
+        this.gamePanel = gamePanel;
 
 
 }
