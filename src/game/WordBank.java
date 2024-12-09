434fda3cb2325196f51cc0c5fed24d3390ec0f8f
MstShaymaIslamPriya <islam23105101055@diu.edu.bd>
2024-12-09 23:07:28 +0600

    wordbank animal

--- src/game/WordBank.java
+++ src/game/WordBank.java
@@ -1,5 +1,11 @@
 package game;
 
-public class WordBank {
+public class WordBank {public String[] animals = {
+    "cat", "dog", "lion", "tiger", "elephant", "giraffe", "zebra", "monkey", "bear", "fox",
+    "wolf", "rabbit", "deer", "panda", "kangaroo", "dolphin", "whale", "shark", "penguin", "owl",
+    "eagle", "sparrow", "peacock", "crocodile", "hippo", "rhino", "leopard", "cheetah", "flamingo", "toucan",
+    "koala", "chameleon", "beetle", "ant", "bee", "butterfly", "snake", "lizard", "buffalo", "hedgehog",
+    "raccoon", "squirrel", "octopus", "starfish", "crab", "seal", "walrus", "mole", "armadillo", "opossum"
+};
 
 }
