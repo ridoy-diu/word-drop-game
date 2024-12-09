4a7f277b253bb68ae7ce1f74a924721addb91771
ri-anik <anik2305101818@diu.edu.bd>
2024-12-09 23:25:27 +0600

    Attribute

--- src/game/ChapterSelection.java
+++ src/game/ChapterSelection.java
@@ -1,5 +1,10 @@
 package game;
+import javax.swing.*;
+import java.awt.*;
 
-public class ChapterSelection {
+public class ChapterSelection extends JPanel {
+    private final JFrame frame;
+    private final WordDropGame gamePanel;
+
 
 }
