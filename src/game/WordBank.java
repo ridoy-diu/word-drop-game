fa34c0bb4358e282ab1c879a0686480259e00eb4
MstShaymaIslamPriya <islam23105101055@diu.edu.bd>
2024-12-09 23:10:10 +0600

    wordbank fruits

--- src/game/WordBank.java
+++ src/game/WordBank.java
@@ -7,5 +7,14 @@ public class WordBank {public String[] animals = {
     "koala", "chameleon", "beetle", "ant", "bee", "butterfly", "snake", "lizard", "buffalo", "hedgehog",
     "raccoon", "squirrel", "octopus", "starfish", "crab", "seal", "walrus", "mole", "armadillo", "opossum"
 };
+public String[] fruits = {
+    "apple", "banana", "cherry", "grape", "orange", "mango", "kiwi", "pineapple", "peach", "pear",
+    "plum", "watermelon", "strawberry", "raspberry", "blueberry", "lemon", "lime", "papaya", "guava", "fig",
+    "coconut", "date", "pomegranate", "apricot", "avocado", "lychee", "dragonfruit", "starfruit", "passionfruit",
+    "cranberry", "elderberry", "jackfruit", "nectarine", "persimmon", "mulberry", "gooseberry", "durian",
+    "longan", "rambutan", "tamarind", "quince", "soursop", "sapodilla", "ugli", "olive", "mandarin",
+    "grapefruit", "boysenberry", "cantaloupe"
+};
+
 
 }
